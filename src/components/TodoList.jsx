8756057bb497303fdd213fa9157620347e648071
Austin Ross <austinlr1014@gmail.com>
2024-08-09 21:03:48 -0500

    I am so lost. Time to double back to JS :)

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
 export default function TodoList() {
+    let todos = {
+      'Go to the gym',
+      'Eat more fruits and veggies',
+      'Pick up the kids from school'
+    }
+  
   return (
-    <div>
-    TodoList
-    </div>
+    <ul>
+      {todos.map((todo, todoIndex) => {
+        return (
+            <li key={todoIndex}>(todo)</li>
+        )
+      })}
+    </ul>
   )
 }
